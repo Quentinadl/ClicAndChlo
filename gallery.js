a53a75b6aee0b3680dc82d00bbc139b1be6cf6dc
Quentin Adeline <quentin.adeline18@gmail.com>
2023-07-07 20:08:07 +0200

    07/07/2023

---
+++ gallery.js
@@ -0,0 +1,34 @@
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+const popupDescription = popup.querySelector('.popup-description');
+
+// Fonction pour afficher l'image en pop-up avec la description
+function showImage(event) {
+  const clickedImage = event.target;
+  const imageSrc = clickedImage.getAttribute('src');
+  const imageAlt = clickedImage.getAttribute('alt');
+  const imageDescription = clickedImage.nextElementSibling.innerHTML;
+
+  const popupImage = popup.querySelector('img');
+  popupImage.src = imageSrc;
+  popupImage.alt = imageAlt;
+
+  popupDescription.innerHTML = imageDescription;
+
+  overlay.style.display = 'flex';
+}
+
+// Fonction pour masquer le pop-up
+function hideImage() {
+  overlay.style.display = 'none';
+}
+
+// Ajouter les écouteurs d'événements pour les images de la galerie
+const images = gallery.querySelectorAll('.image img');
+images.forEach((image) => {
+  image.addEventListener('click', showImage);
+});
+
+// Ajouter un écouteur d'événement pour masquer le pop-up lors du clic sur l'overlay
+overlay.addEventListener('click', hideImage);
